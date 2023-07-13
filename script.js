10c014278fe5b32a3e0094ee3bc23133a2651135
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-07-12 21:25:49 -0500

    Add color modes, toggle lines, reset functionality, and getRandomColor function

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
 // html elements to be used
-const reset = document.getElementById("clear");
 const grid = document.getElementById("grid");
+const defaultMode = document.getElementById("default-mode");
+const randomMode = document.getElementById("random-mode");
+const toggleLines = document.getElementById("toggle-lines");
+const reset = document.getElementById("reset");
 const slider = document.getElementById("myRange");
 const sliderValue = document.getElementById("slider-value");
 
 // variable initialization
 let mouseDown = false; // variable to check if mouse is down is set to false by default and will update to true when mouse is down later in the code
+
 let gridSize = 32; // variable to set grid size to 32 by default
+
 sliderValue.textContent = gridSize + " x " + gridSize; // set slider value to 32 x 32 and shows on page
+
+let colorMode = "default"; // variable to set color mode to defaultColorMode by default
+
+let lines = true; // variable to set lines to true by default
 
 /* event listeners */
 
 // Event listener for mousedown event on the grid
+
 grid.addEventListener("mousedown", function (event) {
     event.preventDefault();
     mouseDown = true; // Set mouseDown variable to true when mouse is pressed down
 });
 
 // Event listener for mouseup event on the grid
+
 grid.addEventListener('mouseup', () => {
     mouseDown = false; // Set mouseDown variable to false when mouse is released
+});
+
+defaultMode.addEventListener("click", function () {
+    colorMode = "default";
+});
+
+randomMode.addEventListener("click", function () {
+    colorMode = "random";
+});
+
+toggleLines.addEventListener("click", toggleDivLines);
+
+function toggleDivLines(){
+    const cells = document.querySelectorAll("#grid > div");
+    if (lines) {
+        cells.forEach(cell => {
+            cell.style.border = "none";
+        });
+        lines = false;
+    }
+    else {
+        cells.forEach(cell => {
+            cell.style.border = "1px solid black";
+        });
+        lines = true;
+    }
+}
+
+reset.addEventListener("click", function () {
+    createGrid(gridSize);
 });
 
 // Slider event listener to change grid size
@@ -29,6 +70,18 @@ slider.oninput = function() {
     createGrid(gridSize);
 }
 
+// This function controls what each color mode does
+
+function setColor(mode, div) {
+    if(mode == "default") {
+        div.style.backgroundColor = "black";
+    }
+    else if(mode == "random") {
+        div.style.backgroundColor = getRandomColor();
+    }
+}
+
+// Function to create the grid
 // Function to create the grid
 function createGrid(size) {
     grid.innerHTML = '';
@@ -40,14 +93,48 @@ function createGrid(size) {
     
     for (i = 0; i < x ; i++) {
         const div = document.createElement("div");
+
+        // Set border style based on value of lines variable
+        if (lines) {
+            div.style.border = "1px solid black";
+        } else {
+            div.style.border = "none";
+        }
+        
         document.getElementById("grid").appendChild(div);
+        
         div.addEventListener("mouseenter", function () {
             // Change the background color of the div to black when the mouse is down
             if (mouseDown) {
-                this.style.backgroundColor = "black";
+                setColor(colorMode, div);
             }
         });
     }
 }
 
+
 createGrid(gridSize); // Call the createGrid function with the initial gridSize value
+
+// Function to toggle grid lines
+
+function toggleDivLines(){
+    const cells = document.querySelectorAll("#grid > div");
+    if (lines) {
+        cells.forEach(cell => {
+            cell.style.border = "none";
+        });
+        lines = false;
+    }
+    else {
+        cells.forEach(cell => {
+            cell.style.border = "1px solid black";
+        });
+        lines = true;
+    }
+}
+
+// Function to generate random color
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
