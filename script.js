f340fab7377bfcababee6f0a47eb4bfb81890871
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-07-13 21:02:56 -0500

    Introduce custom color input, eraser mode, and toggleColorMode function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // html elements to be used
 const grid = document.getElementById("grid");
-const defaultMode = document.getElementById("default-mode");
+//const defaultMode = document.getElementById("default-mode");
+const colorInput = document.getElementById("color");
 const randomMode = document.getElementById("random-mode");
 const toggleLines = document.getElementById("toggle-lines");
+const eraser = document.getElementById("eraser");
 const reset = document.getElementById("reset");
 const slider = document.getElementById("myRange");
 const sliderValue = document.getElementById("slider-value");
@@ -16,11 +18,20 @@ sliderValue.textContent = gridSize + " x " + gridSize; // set slider value to 32
 
 let colorMode = "default"; // variable to set color mode to defaultColorMode by default
 
+let selectedColor = "black"; // variable to set selected color to black by default
+
 let lines = true; // variable to set lines to true by default
 
 /* event listeners */
 
 // Event listener for mousedown event on the grid
+
+// Add event listener for page load
+window.addEventListener("load", function () {
+    // Reset value of color input to default value
+    const colorInput = document.getElementById("color");
+    colorInput.value = "#000000";
+});
 
 grid.addEventListener("mousedown", function (event) {
     event.preventDefault();
@@ -33,13 +44,30 @@ grid.addEventListener('mouseup', () => {
     mouseDown = false; // Set mouseDown variable to false when mouse is released
 });
 
-defaultMode.addEventListener("click", function () {
+/*defaultMode.addEventListener("click", function () {
+    colorMode = "default";
+});*/
+colorInput.addEventListener("input", function () {
+    selectedColor = colorInput.value;
     colorMode = "default";
 });
 
+function toggleColorMode(button, mode) {
+    if (colorMode === "default") {
+        colorMode = mode;
+        button.style.backgroundColor = "lightblue"; // Change color of button when mode is active
+    } else {
+        colorMode = "default";
+        button.style.backgroundColor = ""; // Reset color of button when default mode is active
+    }
+}
+
+// Example usage for randomMode button
 randomMode.addEventListener("click", function () {
-    colorMode = "random";
+    toggleColorMode(randomMode, "random");
 });
+
+
 
 toggleLines.addEventListener("click", toggleDivLines);
 
@@ -59,6 +87,11 @@ function toggleDivLines(){
     }
 }
 
+eraser.addEventListener("click", function () {
+    toggleColorMode(eraser, "eraser");
+});
+
+
 reset.addEventListener("click", function () {
     createGrid(gridSize);
 });
@@ -74,10 +107,13 @@ slider.oninput = function() {
 
 function setColor(mode, div) {
     if(mode == "default") {
-        div.style.backgroundColor = "black";
+        div.style.backgroundColor = selectedColor;
     }
     else if(mode == "random") {
         div.style.backgroundColor = getRandomColor();
+    }
+    else if(mode == "eraser") {
+        div.style.backgroundColor = "white";
     }
 }
 
@@ -100,7 +136,7 @@ function createGrid(size) {
         } else {
             div.style.border = "none";
         }
-        
+
         document.getElementById("grid").appendChild(div);
         
         div.addEventListener("mouseenter", function () {
